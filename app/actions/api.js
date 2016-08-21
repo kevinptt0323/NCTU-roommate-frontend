35a5e0320f519e2907a3f993c7bea71e5049e1fa
Kevin Lin <kevinptt0323@gmail.com>
2016-08-21 18:10:52 +0800

    migrate middleware to redux-thunk

--- app/actions/api.js
+++ app/actions/api.js
@@ -1,8 +1,36 @@
-export const CALL_API = Symbol('CALL_API');
+import request from 'superagent';
+import prefix from 'superagent-prefix';
 
-export const sendAjax = ({withToken = false, ...options}) => ({
-  [CALL_API]: {
-    ...options
-  },
-  withToken
-});
+import { SERVER_HOST } from '../config';
+
+const server = prefix(SERVER_HOST);
+
+export const sendAjax = ({withToken = false, ...options}) => (dispatch, getState) => {
+  const { method, path, query = {}, body, failureType, successType, sendingType } = options;
+
+  dispatch({ type: sendingType });
+
+  if (withToken) {
+    //req.set('Authorization', `Bearer ${token}`);
+    query['token'] = getState().auth.token;
+  }
+
+  let req = request[method](path)
+    .use(server)
+    .query(query)
+    .send(body);
+
+  return req.end((err, res)=> {
+      if (err) {
+        dispatch({
+          type: failureType,
+          response: err
+        });
+      } else {
+        dispatch({
+          type: successType,
+          response: res.body
+        });
+      }
+    });
+};
--- app/stores/configureStore.js
+++ app/stores/configureStore.js
@@ -1,13 +1,14 @@
 import { createStore, applyMiddleware } from 'redux';
 import { hashHistory } from 'react-router';
 import { routerMiddleware } from 'react-router-redux'
+import thunkMiddleware from 'redux-thunk'
 
 import reducer from '../reducers';
 import apiMiddleware from '../middlewares/api';
 
 const INITIAL_STATE = { };
 
-let middlewares = [routerMiddleware(hashHistory), apiMiddleware];
+let middlewares = [routerMiddleware(hashHistory), thunkMiddleware];
 
 export default function configureStore(initialState = INITIAL_STATE) {
   const store = createStore(
