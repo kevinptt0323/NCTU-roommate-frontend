bb719b5dadde4f82cb41fab49d1587c0cc4f0ece
Kevin Lin <kevinptt0323@gmail.com>
2016-08-26 00:17:38 +0800

    composing search result with grid list

--- app/components/SimpleProfile.js
+++ app/components/SimpleProfile.js
@@ -1,11 +1,19 @@
 import React, { PropTypes } from 'react';
+import { Link } from 'react-router';
 
 import Paper from 'material-ui/Paper';
+import { GridTile } from 'material-ui/GridList';
 
-const SimpleProfile = (props, context) => (
-  <Paper>
-    {props.data.student_name}
-  </Paper>
+const SimpleProfile = ({data, ...props}, context) => (
+  <GridTile
+    key={data.student_id}
+    title={data.student_name}
+    actionPosition="left"
+    titlePosition="top"
+    titleBackground="linear-gradient(to bottom, rgba(0,0,0,0.7) 0%,rgba(0,0,0,0.3) 70%,rgba(0,0,0,0) 100%)"
+    containerElement={<Link to={`/user/info/${data.uid}`} />}
+  >
+  </GridTile>
 );
 
 export default SimpleProfile;
--- app/pages/Search.js
+++ app/pages/Search.js
@@ -1,13 +1,27 @@
 import React, { PropTypes } from 'react';
 import { Paper, Tabs, Tab, RaisedButton } from 'material-ui';
+import { GridList } from 'material-ui/GridList';
 import SimpleProfile from '../components/SimpleProfile';
+
+const gridStyle = {
+  maxWidth: '80%',
+  overflowY: 'auto',
+  margin: 'auto'
+};
 
 class Search extends React.Component {
   constructor(props) {
     super(props);
   }
   render() {
-    const searchResult = [{"uid": 1, "facebook_id": "123456789", "student_name": "user1", "detail": "", "email": "user1@example.com", "student_id": "0123456", "student_nickname": "user_nick1", "class_id": 6, "slogan": ""}, {"uid": 2, "facebook_id": "", "student_name": "user2", "detail": "", "email": "user2@example.com", "student_id": "0000000", "student_nickname": "user_nick2", "class_id": 6, "slogan": ""}];
+    const searchResult = [
+      {"uid": 1, "facebook_id": "123456789", "student_name": "user1", "detail": "", "email": "user1@example.com", "student_id": "0123456", "student_nickname": "user_nick1", "class_id": 6, "slogan": ""},
+      {"uid": 2, "facebook_id": "", "student_name": "user2", "detail": "", "email": "user2@example.com", "student_id": "0000000", "student_nickname": "user_nick2", "class_id": 6, "slogan": ""},
+      {"uid": 3, "facebook_id": "123456789", "student_name": "user1", "detail": "", "email": "user1@example.com", "student_id": "0123456", "student_nickname": "user_nick1", "class_id": 6, "slogan": ""},
+      {"uid": 4, "facebook_id": "", "student_name": "user2", "detail": "", "email": "user2@example.com", "student_id": "0000000", "student_nickname": "user_nick2", "class_id": 6, "slogan": ""},
+      {"uid": 5, "facebook_id": "123456789", "student_name": "user1", "detail": "", "email": "user1@example.com", "student_id": "0123456", "student_nickname": "user_nick1", "class_id": 6, "slogan": ""},
+      {"uid": 6, "facebook_id": "", "student_name": "user2", "detail": "", "email": "user2@example.com", "student_id": "0000000", "student_nickname": "user_nick2", "class_id": 6, "slogan": ""},
+    ];
     const searchResultJSX = searchResult.map(profile => (
       <SimpleProfile data={profile} />
     ));
@@ -16,9 +30,13 @@ class Search extends React.Component {
         <div>
           找室友
         </div>
-        <div>
+        <GridList
+          style={gridStyle}
+          cols={4}
+          padding={1}
+        >
           { searchResultJSX }
-        </div>
+        </GridList>
       </div>
     );
   }
