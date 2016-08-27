d7d0abe6e09c0120ef6e18d45b87e557fccf3389
microsheep <twmicrosheep@gmail.com>
2016-08-28 01:20:35 +0800

    change wording

--- app/pages/Login.js
+++ app/pages/Login.js
@@ -40,7 +40,7 @@ class Login extends React.Component {
         >
           <h1 style={{ fontSize: '4em' }}>交大新生</h1>
           <h1 style={{ fontSize: '4em' }}>找小豬系統</h1>
-          <RaisedButton label="D2 登入" secondary={true} href={`${this.context.config.SERVER_HOST}/d2login/`} />
+          <RaisedButton label="尋找室友" secondary={true} href={`${this.context.config.SERVER_HOST}/d2login/`} />
         </div>
       </div>
     );
